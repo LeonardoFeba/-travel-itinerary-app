9f4e4c16e778865d95358dac07697feb28f935db
Leonardo Feba <febaleonardo@gmail.com>
2024-05-27 22:23:40 -0300

    feat: add generate itinerary button and itinerary container

--- src/styled-components/index.js
+++ src/styled-components/index.js
@@ -1,48 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-	FormLabel,
-	PageContainer,
-	SearchInput,
-	StayingDays,
-	Title,
-	Form,
-	ButtonText,
-	SubmitButton,
+  FormLabel,
+  PageContainer,
+  SearchInput,
+  StayingDays,
+  Title,
+  Form,
+  ButtonText,
+  SubmitButton,
+  ContentView,
+  ContainerScroll,
+  ContentTitle,
 } from "./styles";
 import { MaterialIcons } from "@expo/vector-icons";
-import { StatusBar } from "react-native";
+import { Modal, StatusBar, ScrollView, View, Text } from "react-native";
 
 import Slider from "@react-native-community/slider";
 
 export default function Home() {
-	return (
-		<PageContainer>
-			<StatusBar
-				barStyle="dark-content"
-				translucent={true}
-				backgroundColor="#F1F1F1"
-			/>
-			<Title>Roteiro fácil</Title>
-			<Form>
-				<FormLabel>Cidade destino</FormLabel>
-				<SearchInput placeholder="Ex: São Paulo,  SP"></SearchInput>
-				<FormLabel>
-					Tempo de estadia: <StayingDays>10</StayingDays> dias
-				</FormLabel>
-				<Slider
-					minimumValue={1}
-					maximumValue={7} // Número de dias máximo de estadia
-					minimumTrackTintColor="#009688"
-					maximumTrackTintColor="#000000"
-				/>
-			</Form>
-			<SubmitButton>
-				<ButtonText> Gerar roteiro</ButtonText>
-				<MaterialIcons
-					name="travel-explore"
-					size={24}
-					color="#FFF"></MaterialIcons>
-			</SubmitButton>
-		</PageContainer>
-	);
+  // const [modalVisbile, setModalVisibile] = useState(false)
+
+  // function onModal() {
+  // 	return setModalVisibile(true)
+  // }
+
+  return (
+    <PageContainer>
+      <StatusBar
+        barStyle="dark-content"
+        translucent={true}
+        backgroundColor="#F1F1F1"
+      />
+      <Title>Roteiro fácil</Title>
+      <Form>
+        <FormLabel>Cidade destino</FormLabel>
+        <SearchInput placeholder="Ex: São Paulo,  SP"></SearchInput>
+        <FormLabel>
+          Tempo de estadia: <StayingDays>10</StayingDays> dias
+        </FormLabel>
+        <Slider
+          minimumValue={1}
+          maximumValue={7} // Número de dias máximo de estadia
+          minimumTrackTintColor="#009688"
+          maximumTrackTintColor="#000000"
+        />
+      </Form>
+      <SubmitButton>
+        <ButtonText> Gerar roteiro</ButtonText>
+        <MaterialIcons
+          name="travel-explore"
+          size={24}
+          color="#FFF"
+        ></MaterialIcons>
+      </SubmitButton>
+
+      <ContentView>
+        <ContainerScroll>
+          <ContentTitle>Roteiro da sua viagem</ContentTitle>
+        </ContainerScroll>
+      </ContentView>
+
+      {/* <Modal visible={modalVisbile}>
+				<Text>OIIII</Text>
+			</Modal> */}
+    </PageContainer>
+  );
 }
--- src/styled-components/styles.js
+++ src/styled-components/styles.js
@@ -1,55 +1,78 @@
 import styled from "styled-components/native";
 
 export const PageContainer = styled.View`
-	display: flex;
-	flex: 1;
-	background-color: #f1f1f1;
-	align-items: center;
-	padding-top: 54px;
+  display: flex;
+  flex: 1;
+  background-color: #f1f1f1;
+  align-items: center;
+  padding-top: 54px;
 `;
 export const Title = styled.Text`
-	font-size: 32px;
-	font-weight: bold;
+  font-size: 32px;
+  font-weight: bold;
 `;
 export const Form = styled.View`
-	background-color: #fff;
-	width: 95%;
-	border-radius: 8px;
-	padding: 16px;
-	margin-top: 16px;
-	margin-bottom: 8px;
+  background-color: #fff;
+  width: 95%;
+  border-radius: 8px;
+  padding: 16px;
+  margin-top: 16px;
+  margin-bottom: 8px;
 `;
 
 export const FormLabel = styled.Text`
-	font-weight: bold;
-	font-size: 18px;
-	margin-bottom: 8px;
+  font-weight: bold;
+  font-size: 18px;
+  margin-bottom: 8px;
 `;
 
 export const SearchInput = styled.TextInput`
-	border-width: 1px;
-	border-radius: 4px;
-	border-color: #94a3b8;
-	padding: 8px;
-	font-size: 16px;
-	margin-bottom: 16px;
+  border-width: 1px;
+  border-radius: 4px;
+  border-color: #94a3b8;
+  padding: 8px;
+  font-size: 16px;
+  margin-bottom: 16px;
 `;
 
 export const StayingDays = styled.Text`
-	background-color: #f1f1f1;
+  background-color: #f1f1f1;
 `;
 
 export const SubmitButton = styled.TouchableOpacity`
-	display: flex;
-	width: 95%;
-	padding: 14px;
-	flex-direction: row;
-	justify-content: center;
-	align-items: center;
+  display: flex;
+  width: 95%;
+  border-radius: 8px;
+  padding: 14px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: #ff5656;
+  gap: 8px;
 `;
 
 export const ButtonText = styled.Text`
-	font-size: 18px;
-	color: red;
-	font-weight: bold;
+  font-size: 18px;
+  color: red;
+  font-weight: bold;
+`;
+
+export const ContentView = styled.View`
+  background-color: #fff;
+  padding: 16px;
+  width: 100%;
+  margin-top: 16px;
+  border-radius: 8px;
+`;
+
+export const ContentTitle = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 14px;
+`;
+
+export const ContainerScroll = styled.View`
+  width: 95%;
+  margin-top: 8px;
 `;
